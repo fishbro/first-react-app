25a04df47722649089fba00e2faf11cb0f6a34e6
fish_bro <yurariba@gmail.com>
2021-02-04 14:52:33 +0300

    lesson 6.6

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -1,23 +1,22 @@
 import React from "react";
 import classes from './ActiveQuiz.module.scss';
+import AnswersList from '../AnswersList/AnswersList'
 
 const ActiveQuiz = props => (
     <div className={classes.ActiveQuiz}>
         <p className={classes.Question}>
             <span>
                 <strong>2. </strong>
-                How are you?
+                { props.question }
             </span>
 
             <small>4 from 12</small>
         </p>
 
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-        </ul>
+        <AnswersList
+            answers={props.answers}
+            onAnswerClick={props.onAnswerClick}
+        />
     </div>
 )
 
--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -4,16 +4,47 @@ import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz";
 
 class Quiz extends Component{
     state = {
-        quiz: []
+        quiz: [
+            {
+                question: "How are you?",
+                rightAnswerId: 2,
+                answers: [
+                    {
+                        id: 1,
+                        text: 'Very nice'
+                    },
+                    {
+                        id: 2,
+                        text: 'Nice'
+                    },
+                    {
+                        id: 3,
+                        text: 'Sad'
+                    },
+                    {
+                        id: 4,
+                        text: 'Bad'
+                    }
+                ]
+            }
+        ]
+    }
+
+    onAnswerClickHandler = (answerId) => {
+        console.log('Answer Id: ', answerId);
     }
 
     render() {
         return (
             <div className={classes.Quiz}>
                 <div className={classes.QuizWrapper}>
-                    <h1>Quiz</h1>
+                    <h1>Answer the questions</h1>
 
-                    <ActiveQuiz></ActiveQuiz>
+                    <ActiveQuiz
+                        question={this.state.quiz[0].question}
+                        answers={this.state.quiz[0].answers}
+                        onAnswerClick={this.onAnswerClickHandler}
+                    />
                 </div>
             </div>
         );
